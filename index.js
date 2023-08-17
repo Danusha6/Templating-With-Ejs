1b118edd0334ae52530d0687f30661e0d0a9c9a3
Replit user <>
2023-08-17 02:53:01 +0000

    added users page

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var data = require('./data/test.json');
+
 app.set("view engine", "ejs");
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -17,6 +19,7 @@ app.get('/history', (req, res) => {
   res.render("pages/history", { "title": title });
 });
 
+
 app.get('/place', (req, res) => {
   let title = "Places";
   res.render("pages/place", { "title": title });
@@ -27,6 +30,26 @@ app.get('/about', (req, res) => {
   res.render("pages/about", { "title": title });
 });
 
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
